58348ea870d38fe0e09eae55a30311348bf70d36
HenriqueMaquedano <48722849+HenriqueMaquedano@users.noreply.github.com>
2019-03-26 11:49:23 -0300

    Add files via upload

---
+++ inde.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+	<title>
+		Projeto app	
+	</title>
+	<style type="text/css">
+	body{
+		margin: 0;
+		padding: 0px;
+		background-color: #DCDCDC;
+		}
+		.topo{
+		width: 100%;
+		height: 100px;
+		background-color: #e2e2e2
+		}
+		.menu{
+			width: 100%;
+			background-color: #DCDCDC;
+			}
+		.conteudo{
+			width: 70%;
+			margin: o auto;
+			background-color: white;
+			}
+		.rodape{
+			width:100%; height: 150px;
+			background-color: #696969;
+		}	
+		</style>
+		</head>					
+	</head>
+<body>	
+	<div class="topo">
+		Colocar uma imagem do seu projeto	
+	</div>
+	<div class="menu">
+		colocar o menu provisório	
+	</div>
+	<div class="conteudo">
+		Conteúdo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Henrique M. Barbosa<br>
+		<a href="mailto:henriquemcbarboza@gmail.com">Contato</a>	
+	</div>
+</body>	
+</html>
