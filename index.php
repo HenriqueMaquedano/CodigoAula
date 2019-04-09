4d9d94bb617065bd365b2798b13c2640a4cf0a9f
HenriqueMaquedano <48722849+HenriqueMaquedano@users.noreply.github.com>
2019-04-09 11:50:03 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+	<title>
+		Projeto app	
+	</title>
+	<link type="text/css" href="estilo.css"
+rel="stylesheet">
+		</head>					
+<body>	
+	<div class="topo">
+		Colocar uma imagem do seu projeto	
+	</div>
+	<div class="menu">
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="perfil.php">PerfilL</a></li>
+		<li><a href="contato.php">Contato</a></li>
+	</ul>	
+		colocar o menu provisório	
+	</div>
+	<div class="conteudo">
+		Conteúdo principal	
+	</div>
+	<div class="rodape">
+		Desenvolvido por Henrique M. Barbosa<br>
+		<a href="mailto:henriquemcbarboza@gmail.com">Contato</a>	
+	</div>
+</body>	
+</html>
