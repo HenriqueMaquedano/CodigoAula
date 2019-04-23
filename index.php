84e6194dac3c685d500feaf3ab725f78e929b2af
HenriqueMaquedano <48722849+HenriqueMaquedano@users.noreply.github.com>
2019-04-23 11:29:08 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,29 +1,50 @@
-<html>
-	<head>
-	<title>
-		Projeto app	
-	</title>
-	<link type="text/css" href="estilo.css"
-rel="stylesheet">
-		</head>					
-<body>	
-	<div class="topo">
-		Colocar uma imagem do seu projeto	
-	</div>
-	<div class="menu">
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="perfil.php">PerfilL</a></li>
-		<li><a href="contato.php">Contato</a></li>
-	</ul>	
-		colocar o menu provisório	
-	</div>
-	<div class="conteudo">
-		Conteúdo principal	
-	</div>
-	<div class="rodape">
-		Desenvolvido por Henrique M. Barbosa<br>
-		<a href="mailto:henriquemcbarboza@gmail.com">Contato</a>	
-	</div>
-</body>	
-</html>
+<?php
+	include("cabecalho.php");
+?>s
+
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2>Sobre Mim</h2>
+      <h5>Minha Foto:</h5>
+   <div class="fakeimg"> <img src="lobo.jpg" width="170px"height="200px"> </div>
+      <p>Aluno sem criatividade do 3°INFO</p>
+      <h3>Alguns Links</h3>
+      <p>O que Faço?</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Como</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Estudo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Durmo</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Jogar</a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8">
+      <h2>Aula de responssividade</h2>
+      <h5>Framawork Bootstrap 4.1, 16 de Abril de 2019</h5>
+      <div class="fakeimg"> <img src="cachorro.jpg" width="400px"height="200px"></div>
+      <p>Descrição</p>
+      <p>Construção de uma pagina com a utilização do framework do css.</p>
+      <br>
+      <h2>TITLE HEADING</h2>
+      <h5>Title description, Sep 2, 2017</h5>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+  </div>
+</div>
+
+<?php
+	include("rodape.php");
+?>	
+
+
